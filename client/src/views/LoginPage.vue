3ee8a36c2e6bd849508e86f3435507cd852dd31b
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2020-01-04 15:49:51 +0100

    Feat: Login and Sign Up view

---
+++ client/src/views/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="LoginPage">
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from "../components/LoginForm"
+export default {
+  name: 'LoginPage',
+  components:{
+    LoginForm
+  }
+}
+</script>
---
+++ client/src/views/SignupPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="SignupPage">
+    <SignupForm />
+  </div>
+</template>
+
+<script>
+import SignupForm from "../components/SignupForm"
+export default {
+  name: 'LoginPage',
+  components:{
+    SignupForm
+  }
+}
+</script>
