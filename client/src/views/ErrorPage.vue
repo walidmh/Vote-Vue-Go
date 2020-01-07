7948440bb65c591218593695227b1f80a05366a6
walidmh <walid_mohsin10@hotmail.com>
2020-01-07 12:38:18 +0100

    Error Pages
    Signed-off-by: walidmh <walid_mohsin10@hotmail.com>

---
+++ client/src/views/ErrorPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="alert alert-danger" role="alert">
+        Accès non autorisé
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage'
+}
+</script>
+
+<style>
+</style>
---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert alert-danger" role="alert">
+        <p>404 Not Found</p>
+        <p>The page you are looking for is not correct</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style>
+</style>
